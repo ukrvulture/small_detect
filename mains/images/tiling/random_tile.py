ab1fc766d24cf0b729107c1cdd20ea65e9dec96f
odruzhynin <druzhynin.oleksii@gmail.com>
2023-07-27 23:01:42 -0700

    Crop random tile.

---
+++ mains/images/tiling/random_tile.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+from src.img_processing.editing import cropping
+from src.img_processing.tiling import random_tiling
+
+import argparse
+import os
+import pathlib
+import sys
+
+import skimage.io
+
+
+def parse_args(argv):
+    # Parse input arguments.
+    parser = argparse.ArgumentParser(description='Random Tile Cropping')
+    parser.add_argument('-i', '--input_img', dest='input_img_path',
+        help='Path to the input image.', required=True)
+    parser.add_argument('-o', '--output_img', dest='output_img_path',
+        help='Path to the image tile.', required=True)
+
+    parser.add_argument('-w', '--width', dest='tile_width',
+        help='Width of the cropped tile.', required=True, type=int)
+    parser.add_argument('-t', '--height', dest='tile_height',
+        help='Height of the cropped tile.', required=True, type=int)
+
+    return parser.parse_args(argv[1:])
+
+
+def main(argv):
+    parses_args = parse_args(argv)
+    input_img_path = pathlib.Path(parses_args.input_img_path)
+    output_img_path = pathlib.Path(parses_args.output_img_path)
+
+    tile_width = parses_args.tile_width
+    tile_height = parses_args.tile_height
+
+    img_rgba = skimage.io.imread(str(input_img_path))
+    tile_top_left_row, tile_top_left_col = (
+        random_tiling.get_random_tile_row_col(img_rgba, tile_width, tile_height))
+    tile_rgba = cropping.crop_rgba(
+        img_rgba, tile_top_left_row, tile_top_left_col, tile_width, tile_height)
+    skimage.io.imsave(str(output_img_path), tile_rgba)
+
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ src/img_processing/tiling/random_tiling.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# Random tile cropping methods.
+
+import random
+
+
+def get_random_tile_row_col(img_rgba, tile_width, tile_height):
+    """Gets of row and column of the randomly cropped tile.
+
+    Args:
+      img_rgba: Source image RGBa-array.
+      tile_width: Width of cropped tile.
+      tile_height: Height of cropped tile.
+
+    Returns:
+      Row and column offsets of the randomly cropped tile to fit the source image.
+    """
+    img_width = img_rgba.shape[1]
+    img_height = img_rgba.shape[0]
+    if img_width <= tile_width and img_height <= tile_height:
+        return img_rgba
+
+    tile_top_left_row = 0
+    if tile_width < img_width:
+        tile_top_left_row = random.randrange(img_width - tile_width)
+
+    tile_top_left_col = 0
+    if tile_height < img_height:
+        tile_top_left_col = random.randrange(img_height - tile_height)
+
+    return tile_top_left_row, tile_top_left_col
