f4d61d6285a7b18a8fd0ea6f2fd7ff213b25966b
odruzhynin <druzhynin.oleksii@gmail.com>
2023-09-05 23:19:36 -0700

    Add ImageMagick blending.

--- src/img_processing/augmentation/overlay.py
+++ src/img_processing/augmentation/overlay.py
@@ -2,22 +2,30 @@
 
 # Methods for image overlaying.
 
+from src.img_processing.base import image_pool
+
 import logging
 
-import cv2 as cv
 import numpy as np
+import skimage.transform
 
 
-def fit_into_largest(
+class ImageMagicProps:
+    SEAMLESS_SALIENCY_BLEND_AGS = (
+        '1000x0.000002+100')  # Max-IterationsXDistortion+<Print-Iterations>
+
+
+def saliency_blend_into_largest(
     larger_rgba, smaller_rgba, center_row_in_larger, center_col_in_larger,
-    smaller_alpha_mask_thold=None):
-    """Fits smaller image into the larger one.
+    smaller_alpha_mask_thold=None, tiny_img_max_side_size=20):
+    """Blends smaller image into the larger one using ImageMagick algorithms.
 
     Args:
       larger_rgba: Larger image to fit in.
       smaller_rgba: Smaller image to overlay.
       center_row_in_larger: Row-offset of the smaller image center in the largest one.
       center_col_in_larger: Column-offset of the smaller image center in the largest one.
+      tiny_img_max_side_size: Threshold to distinguish too small images.
       smaller_alpha_mask_thold: Alpha channel threshold [0..255] to create a mask of
                                 the smaller image outlining it in the bigger one (optional).
 
@@ -25,40 +33,69 @@ def fit_into_largest(
       Overlaid RGBa-array, smaller image positioned
       in the bigger one before overlay and its binary mask.
     """
-    smaller_rgba_width = smaller_rgba.shape[1]
-    smaller_rgba_height = smaller_rgba.shape[0]
+    with image_pool.ImagePool() as img_pool:
+        smaller_rgba_width = smaller_rgba.shape[1]
+        smaller_rgba_height = smaller_rgba.shape[0]
+        smaller_min_size = min(smaller_rgba_width, smaller_rgba_height)
 
-    top_left_row_in_larger = center_row_in_larger - smaller_rgba_height // 2
-    top_left_col_in_larger = center_col_in_larger - smaller_rgba_width // 2
+        top_left_row_in_larger = center_row_in_larger - smaller_rgba_height // 2
+        top_left_col_in_larger = center_col_in_larger - smaller_rgba_width // 2
 
-    if (top_left_row_in_larger > larger_rgba.shape[1] or
-            top_left_col_in_larger > larger_rgba.shape[1]):
-        logging.warning('Image inserted outside the larger one.')
-        return larger_rgba
+        if (top_left_row_in_larger > larger_rgba.shape[1] or
+                top_left_col_in_larger > larger_rgba.shape[1]):
+            logging.warning('Image inserted outside the larger one.')
+            return larger_rgba
 
-    if top_left_row_in_larger < 0:
-        smaller_rgba = smaller_rgba[-top_left_row_in_larger:, :, :]
-        top_left_row_in_larger = 0
-    if top_left_col_in_larger < 0:
-        smaller_rgba = smaller_rgba[:, -top_left_col_in_larger:, :]
-        top_left_col_in_larger = 0
+        if top_left_row_in_larger < 0:
+            smaller_rgba = smaller_rgba[-top_left_row_in_larger:, :, :]
+            top_left_row_in_larger = 0
+        if top_left_col_in_larger < 0:
+            smaller_rgba = smaller_rgba[:, -top_left_col_in_larger:, :]
+            top_left_col_in_larger = 0
 
-    smaller_rgba = smaller_rgba[
-        :min(smaller_rgba.shape[0], larger_rgba.shape[0] - top_left_row_in_larger),
-        :min(smaller_rgba.shape[1], larger_rgba.shape[1] - top_left_col_in_larger), :]
+        smaller_rgba = smaller_rgba[
+            :min(smaller_rgba.shape[0], larger_rgba.shape[0] - top_left_row_in_larger),
+            :min(smaller_rgba.shape[1], larger_rgba.shape[1] - top_left_col_in_larger), :]
 
-    fit_smaller_rgba = np.zeros(larger_rgba.shape, dtype=np.uint8)
-    fit_smaller_rgba[
-        top_left_row_in_larger:top_left_row_in_larger + smaller_rgba.shape[0],
-        top_left_col_in_larger:top_left_col_in_larger + smaller_rgba.shape[1],
-        :] = smaller_rgba
+        fit_smaller_rgba = np.zeros(larger_rgba.shape, dtype=np.uint8)
+        fit_smaller_rgba[
+            top_left_row_in_larger:top_left_row_in_larger + smaller_rgba.shape[0],
+            top_left_col_in_larger:top_left_col_in_larger + smaller_rgba.shape[1],
+            :] = smaller_rgba
 
-    # noinspection PyUnresolvedReferences
-    blended_rgba = cv.addWeighted(larger_rgba, 1.0, fit_smaller_rgba, 0.7, 0)
+        # blended_rgba = cv.addWeighted(larger_rgba, 1.0, fit_smaller_rgba, 0.7, 0)
 
-    binary_mask = None
-    if smaller_alpha_mask_thold is not None:
-        binary_mask = fit_smaller_rgba[:, :, 3] >= smaller_alpha_mask_thold
-        binary_mask = np.array(binary_mask).astype(np.uint8) * 255
+        src_img = img_pool.imagick_from_rgba(fit_smaller_rgba)
+        target_img = img_pool.imagick_from_rgba(larger_rgba)
 
-    return blended_rgba, fit_smaller_rgba, binary_mask
+        if smaller_min_size > tiny_img_max_side_size:
+            disk_kernel = round(smaller_min_size / 10.0, 1)
+            src_img.morphology(method='erode', kernel=f'disk:{disk_kernel}', channel='alpha')
+            target_img.composite(
+                src_img, operator='saliency_blend',
+                arguments=ImageMagicProps.SEAMLESS_SALIENCY_BLEND_AGS)
+        else:
+            # Average color of source image and use this color to
+            # substitute black transparent pixels.
+            # http://www.github.com/ImageMagick/ImageMagick/discussions/6578#discussioncomment-6899781
+            src_img_to_restore_alpha = img_pool.imagick_from_imagick(src_img)
+            one_pixel_avg_color_rgba = skimage.transform.resize(
+                smaller_rgba, (1, 1), order=1, mode='constant', anti_aliasing=False,
+                preserve_range=True).astype(np.uint8)[0, 0]
+            uniform_avg_color_rgba = np.tile(
+                one_pixel_avg_color_rgba, (larger_rgba.shape[0], larger_rgba.shape[1], 1))
+            transparent_background_to_gradient = img_pool.imagick_from_rgba(
+                uniform_avg_color_rgba)
+            src_img.composite(transparent_background_to_gradient, operator='dst_over')  # gradient
+
+            src_img.composite(src_img_to_restore_alpha, operator='copy_alpha')
+            target_img.composite(
+                src_img, operator='saliency_blend',
+                arguments=ImageMagicProps.SEAMLESS_SALIENCY_BLEND_AGS)
+
+        binary_mask = None
+        if smaller_alpha_mask_thold is not None:
+            binary_mask = fit_smaller_rgba[:, :, 3] >= smaller_alpha_mask_thold
+            binary_mask = np.array(binary_mask).astype(np.uint8) * 255
+
+        return np.array(target_img), fit_smaller_rgba, binary_mask
--- src/img_processing/augmentation/rotation_resizing_cropping.py
+++ src/img_processing/augmentation/rotation_resizing_cropping.py
@@ -2,13 +2,9 @@
 
 # Methods to combine image rotation, resizing and cropping.
 
+from src.img_processing.base import image_pool
+
 import numpy as np
-import scipy.ndimage
-import skimage.color
-import skimage.io
-import skimage.transform
-import skimage.util
-import wand.image
 
 
 def rotate_resize_crop_rgba_img(
@@ -18,57 +14,53 @@ def rotate_resize_crop_rgba_img(
 
     Args:
       img_rgba: Source image RGBa-array.
-      angle_in_degrees: Rotation angle in degrees [0 .. 360].
+      angle_in_degrees: Rotation angle in degrees [0..360].
       scaled_width_in_pixels: Target width in pixels.
-      alpha_channel_threshold: Threshold to filter out transparent pixels [0 .. 255].
+      alpha_channel_threshold: Threshold to filter out transparent pixels [0..255].
 
     Returns:
       RGBa-array with rotated and resized image
       which all the rows and columns have at least one non-transparent pixel.
     """
-    # im = PIL.Image.fromarray(img_rgba)
-    # rotated_img = im.rotate(angle_in_degrees, resample=PIL.Image.BICUBIC, expand=True)
-    # img_rgba = np.array(rotated_img)
+    with image_pool.ImagePool() as img_pool:
+        # img_rgba = scipy.ndimage.rotate(img_rgba, angle=angle_in_degrees, reshape=True)
+        rotated_img = img_pool.imagick_from_rgba(img_rgba)
+        rotated_img.rotate(-angle_in_degrees)
+        img_rgba = np.array(rotated_img)
 
-    # convert tank06.png -background 'rgba(0,0,0,0)'   -filter Lanczos  -rotate -30  -resize 9%    tank06.resized.png
-    rotated_img = wand.image.Image.from_array(img_rgba, channel_map='RGBA')
-    rotated_img.rotate(-angle_in_degrees)
-    img_rgba = np.array(rotated_img)
+        # Clear boundary artifacts.
+        transparent_pixels = img_rgba[:, :, 3] < alpha_channel_threshold
 
-    # img_rgba = scipy.ndimage.rotate(img_rgba, angle=angle_in_degrees, reshape=True)
+        visible_min_r, visible_max_r = 0, img_rgba.shape[0] - 1
+        visible_min_c, visible_max_c = 0, img_rgba.shape[1] - 1
 
-    # Filter .
-    transparent_pixels = img_rgba[:, :, 3] < alpha_channel_threshold
+        while np.all(transparent_pixels[visible_min_r, :]):
+            visible_min_r += 1
+        while np.all(transparent_pixels[visible_max_r, :]):
+            visible_max_r -= 1
+        while np.all(transparent_pixels[:, visible_min_c]):
+            visible_min_c += 1
+        while np.all(transparent_pixels[:, visible_max_c]):
+            visible_max_c -= 1
 
-    visible_min_r, visible_max_r = 0, img_rgba.shape[0] - 1
-    visible_min_c, visible_max_c = 0, img_rgba.shape[1] - 1
+        rotated_and_resized_rgba = np.copy(img_rgba[
+            visible_min_r:visible_max_r + 1, visible_min_c:visible_max_c + 1,:])
 
-    while np.all(transparent_pixels[visible_min_r, :]):
-        visible_min_r += 1
-    while np.all(transparent_pixels[visible_max_r, :]):
-        visible_max_r -= 1
-    while np.all(transparent_pixels[:, visible_min_c]):
-        visible_min_c += 1
-    while np.all(transparent_pixels[:, visible_max_c]):
-        visible_max_c -= 1
+        scaled_height_in_pixels = int(rotated_and_resized_rgba.shape[0] *
+            scaled_width_in_pixels / rotated_and_resized_rgba.shape[1])
 
-    rotated_and_resized_rgba = np.copy(img_rgba[
-        visible_min_r:visible_max_r + 1, visible_min_c:visible_max_c + 1,:])
+        # rotated_and_resized_rgba = skimage.transform.resize(
+        #    rotated_and_resized_rgba, (scaled_height_in_pixels, scaled_width_in_pixels),
+        #    order=1, mode='constant', anti_aliasing=False,
+        #    preserve_range=True).astype(np.uint8)
+        rotated_and_resized_img = img_pool.imagick_from_rgba(rotated_and_resized_rgba)
+        rotated_and_resized_img.resize(
+            width=scaled_width_in_pixels, height=scaled_height_in_pixels, filter='lanczos')
+        rotated_and_resized_rgba = np.array(rotated_and_resized_img)
 
-    scaled_height_in_pixels = int(rotated_and_resized_rgba.shape[0] *
-        scaled_width_in_pixels / rotated_and_resized_rgba.shape[1])
+        rotated_and_resized_rgba[
+           rotated_and_resized_rgba[:, :, 3] < alpha_channel_threshold] = [0, 0, 0, 0]
+        rotated_and_resized_rgba[
+           rotated_and_resized_rgba[:, :, 3] >= alpha_channel_threshold, 3] = 255
 
-    rotated_and_resized_img = wand.image.Image.from_array(rotated_and_resized_rgba, channel_map='RGBA')
-    rotated_and_resized_img.resize(width=scaled_width_in_pixels, height=scaled_height_in_pixels, filter='lanczos')
-    rotated_and_resized_rgba = np.array(rotated_and_resized_img)
-    # rotated_and_resized_rgba = skimage.transform.resize(
-    #    rotated_and_resized_rgba, (scaled_height_in_pixels, scaled_width_in_pixels),
-    #    order=1, mode='constant', anti_aliasing=False,
-    #    preserve_range=True).astype(np.uint8)
-
-    rotated_and_resized_rgba[
-       rotated_and_resized_rgba[:, :, 3] < alpha_channel_threshold] = [0, 0, 0, 0]
-    rotated_and_resized_rgba[
-       rotated_and_resized_rgba[:, :, 3] >= alpha_channel_threshold, 3] = 255
-
-    return rotated_and_resized_rgba
+        return rotated_and_resized_rgba
