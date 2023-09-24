c73ff3ba48e0fd9d89e057403cf2e35c0b0f5292
odruzhynin <druzhynin.oleksii@gmail.com>
2023-09-23 18:28:22 -0700

    Add padding to augmented image tile.

--- src/img_processing/io/imgread.py
+++ src/img_processing/io/imgread.py
@@ -6,28 +6,37 @@ from src.img_processing.base import image
 
 import logging
 import os
+import re
 
 import imghdr
 import numpy as np
 import skimage.io
 
 
-def list_image_file_from_dir(dir_path, ignore_non_images=False, recursive=False):
+def list_image_file_from_dir(
+    dir_path, recursive=False, ignore_non_images=False, ignored_file_regexp=None):
     """Lists absolute paths to image files.
 
     Args:
       dir_path: Root folder path.
-      ignore_non_images: Just skip, if there is an auxiliary files in the folder.
       recursive: Flag to look also in sub-folders.
+      ignore_non_images: Just skip, if there is an auxiliary files in the folder.
+      ignored_file_regexp: Python compiled regexp pattern or sting of ignored file names.
 
     Returns:
       Generator yielding ImageFile objects.
     """
+    if ignored_file_regexp and isinstance(ignored_file_regexp, str):
+        skip_file_regexp = re.compile(ignored_file_regexp)
+
     sub_paths = []
     for file_name in sorted(os.listdir(str(dir_path))):
         file_path = dir_path / file_name
         if file_path.is_dir():
             sub_paths.append(file_path)
+            continue
+
+        if ignored_file_regexp and ignored_file_regexp.match(file_name):
             continue
 
         if imghdr.what(str(file_path)):
@@ -40,13 +49,13 @@ def list_image_file_from_dir(dir_path, ignore_non_images=False, recursive=False)
             yield from list_image_file_from_dir(dir_path, ignore_non_images, recursive)
 
 
-def load_images_from_dir(dir_path, ignore_non_images=False, recursive=False):
+def load_images_from_dir(dir_path, recursive=False, ignore_non_images=False):
     """Loads images from disk skipping auxiliary files.
 
     Args:
       dir_path: Folder path.
-      ignore_non_images: Just skip, if there is an auxiliary files in the folder.
       recursive: Flag to look also in sub-folders.
+      ignore_non_images: Just skip, if there is an auxiliary files in the folder.
 
     Returns:
       Generator yielding RawImage objects.
--- src/machine_learning/datasets/augmentation/src_rotate_resize_to_target_main.py
+++ src/machine_learning/datasets/augmentation/src_rotate_resize_to_target_main.py
@@ -12,16 +12,18 @@
 
 from src.img_processing.augmentation import adjust_overlay
 from src.img_processing.augmentation import rotate_resize_crop
+from src.img_processing.base import image_parts
 from src.img_processing.editing import cropping
 from src.img_processing.editing import random_selection
 from src.img_processing.io import imgread
-from src.img_processing.tiling import random_tile
+from src.img_processing.tiling import tile_breaking
 
 import argparse
 import collections
 import json
 import logging
 import os
+import re
 import pathlib
 import random
 import shutil
@@ -51,39 +53,43 @@ class AugmentedSetInputs:
 
     @property
     def output_img_suffix(self):
-        return f'.a{self.angle_in_degrees}.w{self.scaled_width_in_pixels}'
+        return f'.a{self.angle_in_degrees:03}.w{self.scaled_width_in_pixels:03}'
 
 
 def parse_args(argv):
     # Parse input arguments.
     parser = argparse.ArgumentParser(description='Fit Sources into Targets')
     parser.add_argument('-s', '--src_dir', dest='src_dir_path',
-        help='Folder with source object images.', required=True)
+                        help='Folder with source object images.', required=True)
     parser.add_argument('-d', '--targets_dir', dest='targets_dir_path',
-        help='Folder with target images.', required=True)
+                        help='Folder with target images.', required=True)
     parser.add_argument('-o', '--output_dir', dest='output_dir_path',
-        help='Output folder.', required=True)
+                        help='Output folder.', required=True)
 
     parser.add_argument('-n', '--num_outputs', dest='num_of_outputs',
-        help='Total number of augmented images.', required=False,
-        type=int, default=1)
+                        help='Total number of augmented images.', required=False,
+                        type=int, default=1)
     parser.add_argument('-i', '--tiles_per_img', dest='num_tiles_per_image',
-        help='Number of cropped tiles per one target image.', required=False,
-        type=int, default=4)
+                        help='Number of cropped tiles per one target image.', required=False,
+                        type=int, default=4)
+
+    parser.add_argument('-p', '--target_paddings', dest='target_paddings',
+                        help='Comma-separated left, right, top, bottom target image paddings.',
+                        required=False, default="0,0,0,0")
 
     parser.add_argument('-w', '--tile_width', dest='tile_width',
-        help='Width of the cropped tile.', required=True, type=int)
+                        help='Width of the cropped tile.', required=True, type=int)
     parser.add_argument('-t', '--tile_height', dest='tile_height',
-        help='Height of the cropped tile.', required=True, type=int)
+                        help='Height of the cropped tile.', required=True, type=int)
 
-    parser.add_argument('-l', '--low_obj_width', dest='low_object_width',
-        help='Low boundary of augmented object width.', required=True, type=int)
-    parser.add_argument('-u', '--upper_obj_width', dest='upper_object_width',
-        help='Height boundary of augmented object width.', required=True, type=int)
+    parser.add_argument('-l', '--low_src_width', dest='low_src_obj_width',
+                        help='Low boundary of augmented object width.', required=True, type=int)
+    parser.add_argument('-u', '--upper_src_width', dest='upper_src_obj_width',
+                        help='Height boundary of augmented object width.', required=True, type=int)
 
     parser.add_argument('-a', '--alpha_threshold', dest='alpha_channel_threshold',
-        help='Transparency threshold for smoothing [0..255].', required=False,
-        type=int, default=230)
+                        help='Transparency threshold for smoothing [0..255].', required=False,
+                        type=int, default=230)
 
     return parser.parse_args(argv[1:])
 
@@ -94,10 +100,10 @@ def main(argv):
     targets_dir_path = pathlib.Path(parsed_args.targets_dir_path)
     output_dir_path = pathlib.Path(parsed_args.output_dir_path)
     if not src_dir_path.is_dir() or not targets_dir_path.is_dir():
-        logging.error(f'{src_dir_path} or {targets_dir_path} is not a dir.')
+        logging.error('%s or %s is not a dir.', src_dir_path, targets_dir_path)
         return os.EX_NOINPUT
     if output_dir_path.is_file():
-        logging.error(f'{output_dir_path} is a file.')
+        logging.error('%s is a file.', output_dir_path)
         return os.EX_NOINPUT
 
     shutil.rmtree(output_dir_path)
@@ -115,13 +121,18 @@ def main(argv):
         logging.error('Tile width or height is negative.')
         return os.EX_NOINPUT
 
-    lower_bound_of_object_width = parsed_args.low_object_width
-    upper_bound_of_object_width = parsed_args.upper_object_width
-    if not (0 < lower_bound_of_object_width < tile_width and
-            0 < upper_bound_of_object_width < tile_width):
+    target_paddings = image_parts.Paddings.parse_from_csv(parsed_args.target_paddings)
+    if not target_paddings:
+        logging.error('Invalid %s paddings.', target_paddings)
+        return os.EX_NOINPUT
+
+    lower_bound_of_src_obj_width = parsed_args.low_src_obj_width
+    upper_bound_of_src_obj_width = parsed_args.upper_src_obj_width
+    if not (0 < lower_bound_of_src_obj_width < tile_width and
+            0 < upper_bound_of_src_obj_width < tile_width):
         logging.error('Tile width or height is negative.')
         return os.EX_NOINPUT
-    if lower_bound_of_object_width > upper_bound_of_object_width:
+    if lower_bound_of_src_obj_width > upper_bound_of_src_obj_width:
         logging.error('Lower bound of augmented object width is greater then the upper one.')
         return os.EX_NOINPUT
 
@@ -130,9 +141,13 @@ def main(argv):
         logging.error('Incorrect source object transparency threshold.')
         return os.EX_NOINPUT
 
-    target_image_files = collections.deque(
-        imgread.list_image_file_from_dir(targets_dir_path))
-    src_obj_img_files = list(imgread.list_image_file_from_dir(src_dir_path))
+    labeling_file_regexp = re.compile(r'(.*\.ini|.*\.xcf|.*\.psd)')
+    target_image_files = collections.deque(imgread.list_image_file_from_dir(
+        targets_dir_path, recursive=True,
+        ignore_non_images=True, ignored_file_regexp=labeling_file_regexp))
+    src_obj_img_files = list(imgread.list_image_file_from_dir(
+        src_dir_path, recursive=True,
+        ignore_non_images=True, ignored_file_regexp=labeling_file_regexp))
 
     aug_set_inputs = AugmentedSetInputs()
 
@@ -142,16 +157,23 @@ def main(argv):
         target_image = target_image_file.load()
         target_image_files.rotate(-1)
         if not target_image:
-            logging.error("Malformed target %s.", target_image_files[0])
+            logging.error('Malformed target %s.', target_image_files[0])
             target_image_files.pop()  # malformed
+            continue
+
+        if not (target_paddings.within_width(target_image.shape[1]) and
+                target_paddings.within_height(target_image.shape[0])):
+            logging.error('%s padding mismatch %s.', target_paddings, target_image_file)
+            target_image_files.pop()
             continue
 
         img_tile_idx = 0
         while img_tile_idx < num_tiles_per_image and src_obj_img_files:
             src_obj_img_file = random.choice(src_obj_img_files)
             src_obj_img = src_obj_img_file.load()
-            if not src_obj_img:
-                logging.error("Malformed source %s.", src_obj_img_file)
+            if (not src_obj_img or not tile_breaking.any_tile_fit(
+                    (tile_height, tile_width), target_image.shape, target_paddings)):
+                logging.error('Malformed or too small source %s.', src_obj_img_file)
                 src_obj_img_files.remove(src_obj_img_file)
                 continue
             img_tile_idx += 1
@@ -160,12 +182,13 @@ def main(argv):
                 target_img_path=target_image_file.path, source_img_path=src_obj_img_file.path)
 
             aug_set_inputs.tile_top_left_row, aug_set_inputs.tile_top_left_col = (
-                random_tile.get_random_tile_row_col(target_image.shape, tile_width, tile_height))
+                tile_breaking.get_random_tile_row_col(
+                    (tile_height, tile_width), target_image.shape, target_paddings))
             aug_set_inputs.tile_width, aug_set_inputs.tile_height = tile_width, tile_height
             aug_set_inputs.angle_in_degrees = random.randint(0, 180)
             aug_set_inputs.scaled_width_in_pixels = random.randint(
-                min(src_obj_img.shape[1], lower_bound_of_object_width),
-                min(src_obj_img.shape[1], upper_bound_of_object_width) + 1)
+                min(src_obj_img.shape[1], lower_bound_of_src_obj_width),
+                min(src_obj_img.shape[1], upper_bound_of_src_obj_width) + 1)
 
             # noinspection PyBroadException
             try:
@@ -173,7 +196,7 @@ def main(argv):
                     target_image, src_obj_img, alpha_channel_threshold,
                     aug_set_inputs, output_dir_path)
             except Exception:
-                logging.exception(f"Augmentation {aug_set_inputs}.")
+                logging.exception('Augmentation %s.', aug_set_inputs)
 
             output_idx += 1
             if output_idx % 50 == 0:
@@ -182,9 +205,9 @@ def main(argv):
                 break
 
     if not target_image_files or not src_obj_img_files:
-        logging.warning("No target or source images.")
+        logging.warning('No target or source images.')
     if output_idx % 50 != 0:
-        logging.info(f"{output_idx} output sets generated.")
+        logging.info('%d output sets generated.', output_idx)
     return os.EX_OK
 
 
@@ -232,8 +255,9 @@ def augment_source_in_target(
         target_image.path.stem + aug_set_inputs.output_img_suffix + '.augsrc.png')
     binary_mask_path = output_dir_path.joinpath(
         target_image.path.stem + aug_set_inputs.output_img_suffix + '.mask.png')
-    skimage.io.imsave(str(target_with_augmented_img_path), target_with_augmented_rgba)
-    skimage.io.imsave(str(target_tile_path), tile_rgba)
+    skimage.io.imsave(
+        str(target_with_augmented_img_path), target_with_augmented_rgba, check_contrast=False)
+    skimage.io.imsave(str(target_tile_path), tile_rgba, check_contrast=False)
     skimage.io.imsave(str(augmented_src_path), augmented_src_rgba, check_contrast=False)
     skimage.io.imsave(
         str(binary_mask_path), skimage.img_as_uint(binary_mask), check_contrast=False)
